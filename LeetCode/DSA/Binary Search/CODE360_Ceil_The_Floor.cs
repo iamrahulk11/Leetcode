d164d6177bddaed327fbd6038c12b5f484bd1680
rahul <rahul.kumar3@mastertrust.co.in>
2024-09-13 16:41:48 +0530

    1. Added LC33_Search_in_Rotated_Sorted_Array 2. Added CODE360_Ceil_The_Floor

---
+++ LeetCode/DSA/Binary Search/CODE360_Ceil_The_Floor.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DSA.Binary_Search
+{
+    internal class CODE360_Ceil_The_Floor
+    {
+        public static void Main(string[] args)
+        {
+            int n = 10, target = 14;
+            int[] nums = { 6, 6, 7, 12, 16, 18, 19, 22, 23, 30 };
+
+            Console.WriteLine("Result: " + ceil(nums, n, target));
+            Console.ReadLine();
+        }
+
+        public static (int,int) ceil(int[] a, int n, int x)
+        {
+            int start = 0, end = n - 1;
+            int answer = -1;
+            (int, int) answerPair;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (a[mid] <= x)
+                {
+                    answer = a[mid];
+                    start = mid + 1;                    
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            answerPair.Item1 = answer;
+
+            start = 0; end = n - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                if (a[mid] >= x)
+                {
+                    answer = a[mid];
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            answerPair.Item2 = answer;
+            return answerPair;
+        }
+    }
+}
---
+++ LeetCode/DSA/Binary Search/LC33_Search_in_Rotated_Sorted_Array.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DSA.Binary_Search
+{
+    internal class LC33_Search_in_Rotated_Sorted_Array
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 4, 5, 6, 7, 0, 1, 2 };
+            int target = 0;
+
+            Console.WriteLine("Result : " + Search(nums, target));
+
+            Console.ReadLine();
+        }
+        public static int Search(int[] nums, int target)
+        {
+            if (nums[0] == target)
+            {
+                return 0;
+            }
+            if (nums[nums.Length - 1] == target)
+            {
+                return nums.Length - 1;
+            }
+
+            int start = 0, end = nums.Length - 1;
+            while (start <= end)
+            {
+                if (target == nums[start])
+                {
+                    return start;
+                }
+                else if (target == nums[end])
+                {
+                    return end;
+                }
+                int mid = (start + end) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+                if (nums[mid] >= nums[start])
+                {
+                    if (nums[mid] >= target && nums[start] <= target)
+                    {
+                        end = mid - 1;
+                    }
+                    else
+                    {
+                        start = mid + 1;
+                    }
+                }
+                else
+                {
+                    if (nums[mid] <= target && nums[end] >= target)
+                    {
+                        start = mid + 1;
+                    }
+                    else
+                    {
+                        end = mid - 1;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
