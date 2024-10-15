654b1faac4b8a5d149f58bd00b20b8656e0fa4b7
rahul <rahul.kumar3@mastertrust.co.in>
2024-10-15 17:37:10 +0530

    1. Added Separate_Black_and_White_Balls

---
+++ LeetCode/LeetCode/Separate_Black_and_White_Balls.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal class Separate_Black_and_White_Balls
+    {
+        public static void Main(string[] args)
+        {
+            string s = "";
+            Console.WriteLine(MinimumSteps(s));
+        }
+        public static long MinimumSteps(string s)
+        {
+            long countZero = 0;
+            long steps = 0;
+            for (int i = s.Length - 1; i >= 0; i++)
+            {
+                if (s[i].ToString() == "0")
+                {
+                    countZero++;
+                }
+                if (s[i].ToString() == "1")
+                {
+                    steps = steps + countZero;
+                }
+            }
+            return steps;
+        }
+    }
+}
