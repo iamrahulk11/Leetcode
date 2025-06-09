e016126edb581a1acdc883fd78774f293fd215d5
rahulkumar <rahul.kumar@skylabstech.com>
2025-06-09 23:59:51 +0530

    1. Number of island 2. Max area of the island 3. Surrounded Regions

---
+++ LeetCode/LeetCode/NumberOfIsland.cs
@@ -0,0 +1,141 @@
+﻿namespace LeetCode
+{
+    // Two Problem Solution : Number of island and Max area of the island
+    class NumberOfIsland
+    {
+        public static void Main()
+        {
+            //int[][] grid = [
+            //                [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], 
+            //                [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], 
+            //                [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], 
+            //                [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0], 
+            //                [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0], 
+            //                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], 
+            //                [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0], 
+            //                [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0]
+            //                ];
+
+            int[][] grid = [[1]];
+
+            Console.WriteLine(NumIslands(grid));
+
+        }
+        public static int NumIslands(int[][] grid)
+        {
+            int n = grid.Length;
+            int m = grid[0].Length;
+
+            int[,] isVisited = new int[n, m];
+            int largestArea = 0;
+            int currentArea = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    if (grid[row][col] == 1 && isVisited[row, col] == 0)
+                    {
+                        largestArea = 1;
+                        currentArea = bfsMapVisitedFourDirection(row, col, isVisited, grid);
+                        largestArea = Math.Max(largestArea, currentArea);
+                    }
+                }
+            }
+            return largestArea;
+        }
+        public class Pair
+        {
+            public int first;
+            public int second;
+            public Pair(int firstValue, int secondValue)
+            {
+                this.first = firstValue;
+                this.second = secondValue;
+            }
+        }        
+
+        // 4 directions
+        private static int bfsMapVisitedFourDirection(int row, int col, int[,] isVisited, int[][] grid)
+        {
+            int n = grid.Length;
+            int m = grid[0].Length;
+
+            Queue<Pair> pairs = new();
+            pairs.Enqueue(new Pair(row, col));
+
+            int area = 0;
+
+            while (pairs.Count > 0)
+            {
+                int nrow = pairs.Peek().first;
+                int ncol = pairs.Peek().second;
+                pairs.Dequeue();
+
+                if ((nrow - 1) >= 0 && grid[nrow - 1][ncol] == 1 && isVisited[nrow - 1, ncol] == 0)
+                {
+                    isVisited[nrow - 1, ncol] = 1;
+                    pairs.Enqueue(new Pair(nrow - 1, ncol));
+                    area++;
+                }
+                if ((ncol + 1) < m && grid[nrow][ncol + 1] == 1 && isVisited[nrow, ncol + 1] == 0)
+                {
+                    isVisited[nrow, ncol + 1] = 1;
+                    pairs.Enqueue(new Pair(nrow, ncol + 1));
+                    area++;
+                }
+                if ((nrow + 1) < n && grid[nrow + 1][ncol] == 1 && isVisited[nrow + 1, ncol] == 0)
+                {
+                    isVisited[nrow + 1, ncol] = 1;
+                    pairs.Enqueue(new Pair(nrow + 1, ncol));
+                    area++;
+                }
+                if ((ncol - 1) >= 0 && grid[nrow][ncol - 1] == 1 && isVisited[nrow, ncol - 1] == 0)
+                {
+                    isVisited[nrow, ncol - 1] = 1;
+                    pairs.Enqueue(new Pair(nrow, ncol - 1));
+                    area++;
+                }
+
+            }
+            if(area == 0)
+            {
+                return 1;
+            }
+            return area;
+        }
+
+        // 8 directions
+        private static void bfsMapVisited(int row, int col, int[,] isVisited, int[][] grid)
+        {
+            int n = grid.Length;
+            int m = grid[0].Length;
+
+            Queue<Pair> pairs = new();
+            pairs.Enqueue(new Pair(row, col));
+
+            while (pairs.Count > 0)
+            {
+                int nrow = pairs.Peek().first;
+                int ncol = pairs.Peek().second;
+                pairs.Dequeue();
+
+                for (int i = -1; i <= 1; i++)
+                {
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        int rotatedRow = nrow + i;
+                        int rotatedCol = ncol + j;
+
+                        if (rotatedRow >= 0 && rotatedRow < n && rotatedCol >= 0 && rotatedCol < m &&
+                            grid[rotatedRow][rotatedCol] == 1 && isVisited[rotatedRow, rotatedCol] == 0)
+                        {
+                            isVisited[rotatedRow, rotatedCol] = 1;
+                            pairs.Enqueue(new Pair(rotatedRow, rotatedCol));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Surrounded_Regions.cs
@@ -0,0 +1,114 @@
+﻿namespace LeetCode
+{
+    class Surrounded_Regions
+    {
+        public static void Main()
+        {
+            //char[][] grid = [['X', 'X', 'X', 'X'], ['X', 'O', 'O', 'X'], ['X', 'X', 'O', 'X'], ['X', 'O', 'X', 'X']];
+            //char[][] grid = [['O', 'O'], ['O', 'O']];
+            char[][] grid = [['O', 'O', 'O'], ['O', 'O', 'O'], ['O', 'O', 'O']];
+
+            Solve(grid);
+
+            foreach (var item in grid)
+            {
+                foreach (var val in item)
+                {
+                    Console.Write(val);
+                    Console.Write(" ");
+                }
+            }
+            Console.ReadLine();
+
+        }
+        public static void Solve(char[][] board)
+        {
+            int n = board.Length;
+            int m = board[0].Length;
+
+            int[,] isVisited = new int[n, m];
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < m; col++)
+                {
+                    if (board[row][col] == 'O' && isVisited[row, col] == 0)
+                    {
+                        bfs(row, col, board, isVisited);
+                    }
+                }
+            }
+        }
+        public class Pair
+        {
+            public int first;
+            public int second;
+            public Pair(int firstValue, int secondValue)
+            {
+                this.first = firstValue;
+                this.second = secondValue;
+            }
+        }
+
+        // 4 directions
+        // keep the tract of visited pairs and a flag which ensure the chain is connected to the edges or not
+        // Note : needs to be optimize
+        private static void bfs(int row, int col, char[][] board, int[,] isVisited)
+        {
+            int n = board.Length;
+            int m = board[0].Length;
+
+            Queue<Pair> pairs = new();
+            Queue<Pair> Savedpairs = new();
+            pairs.Enqueue(new Pair(row, col));
+            Savedpairs.Enqueue(new Pair(row, col));
+
+            bool isEdge = false;
+
+            while (pairs.Count > 0)
+            {
+                int prow = pairs.Peek().first;
+                int pcol = pairs.Peek().second;
+                pairs.Dequeue();
+
+                if ((prow - 1) >= 0 && board[prow - 1][pcol] == 'O' && isVisited[prow - 1, pcol] == 0)
+                {
+                    isVisited[prow - 1, pcol] = 1;
+                    pairs.Enqueue(new Pair(prow - 1, pcol));
+                    Savedpairs.Enqueue(new Pair(prow - 1, pcol));
+                }
+                if ((pcol + 1) < m && board[prow][pcol + 1] == 'O' && isVisited[prow, pcol + 1] == 0)
+                {
+                    isVisited[prow, pcol + 1] = 1;
+                    pairs.Enqueue(new Pair(prow, pcol + 1));
+                    Savedpairs.Enqueue(new Pair(prow, pcol + 1));
+                }
+                if ((prow + 1) < n && board[prow + 1][pcol] == 'O' && isVisited[prow + 1, pcol] == 0)
+                {
+                    isVisited[prow + 1, pcol] = 1;
+                    pairs.Enqueue(new Pair(prow + 1, pcol));
+                    Savedpairs.Enqueue(new Pair(prow + 1, pcol));
+                }
+                if ((pcol - 1) >= 0 && board[prow][pcol - 1] == 'O' && isVisited[prow, pcol - 1] == 0)
+                {
+                    isVisited[prow, pcol - 1] = 1;
+                    pairs.Enqueue(new Pair(prow, pcol - 1));
+                    Savedpairs.Enqueue(new Pair(prow, pcol - 1));
+                }
+                if (prow == 0 || prow == (n - 1) || pcol == 0 || pcol == (m - 1) && board[prow][pcol] == 'O')
+                {
+                    isEdge = true;
+                }
+                
+            }
+
+            if (!isEdge)
+            {
+                foreach (var item in Savedpairs)
+                {
+                    board[item.first][item.second] = 'X';
+                }
+            }
+        }
+    }
+}
